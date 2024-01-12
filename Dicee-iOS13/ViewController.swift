81696b4e614f93b35235c258136a1cd17a0954d5
David Vazquez <dave_u@outlook.com>
2024-01-11 19:27:49 -0600

    first commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //IBOutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!	
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceSix") ]
+        
+        diceImageViewOne.image = diceArray.randomElement() // Get a random element of the actual array
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
+
+    
+        
     }
-
-
 }
 
